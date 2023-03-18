ea75d2d41e19888cf5222d0597716255ae5a1096
sksat <sksat@sksat.net>
2023-03-19 06:32:48 +0900

    discord: transfer owner to sksat

--- terraform/discord/discord.tf
+++ terraform/discord/discord.tf
@@ -24,6 +24,7 @@ provider "discord" {
 resource "discord_server" "pasokonistan" {
   name          = "パソコニスタン"
   region        = "japan"
+  owner_id      = data.discord_member.sksat.id
   icon_data_uri = data.discord_local_image.logo.data_uri
 }
 data "discord_local_image" "logo" {
