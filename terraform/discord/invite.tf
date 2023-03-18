6497958be8f5155ca8960973ae8c51219dca20d7
sksat <sksat@sksat.net>
2023-03-19 06:22:11 +0900

    discord: add main invite link

--- terraform/discord/invite.tf
+++ terraform/discord/invite.tf
@@ -7,3 +7,12 @@ output "invite_link_bootstrap" {
   description = "bootstrap invite link"
   value       = "https://discord.gg/${discord_invite.bootstrap.id}"
 }
+
+resource "discord_invite" "main" {
+  channel_id = discord_text_channel.entrypoint.id
+  max_uses   = 100
+}
+output "invite_link_main" {
+  description = "main invite link"
+  value       = "https://discord.gg/${discord_invite.main.id}"
+}
