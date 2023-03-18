86cbe7e445f357259dca4188e955cdff5ad5efeb
sksat <sksat@sksat.net>
2023-03-18 12:56:11 +0900

    discord: add vc_text

--- terraform/discord/voice.tf
+++ terraform/discord/voice.tf
@@ -4,9 +4,14 @@ resource "discord_category_channel" "voice" {
   position  = 1
 }
 
-resource "discord_voice_channel" "general" {
-  name      = "vc"
+resource "discord_voice_channel" "vc" {
   server_id = discord_server.pasokonistan.id
   category  = discord_category_channel.voice.id
+  name      = "vc"
   position  = 0
+}
+resource "discord_text_channel" "vc_text" {
+  server_id = discord_server.pasokonistan.id
+  category  = discord_category_channel.voice.id
+  name      = "vc_text"
 }
