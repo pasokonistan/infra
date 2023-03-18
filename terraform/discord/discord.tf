41ee986ef46bc39c0bd5f65c8f5aa0d8366b4596
sksat <sksat@sksat.net>
2023-03-18 12:19:58 +0900

    discord: add entrypoint channel in pasokon

--- terraform/discord/discord.tf
+++ terraform/discord/discord.tf
@@ -31,3 +31,10 @@ resource "discord_category_channel" "pasokon" {
   server_id = discord_server.pasokonistan.id
   position  = 0
 }
+
+resource "discord_text_channel" "entrypoint" {
+  name      = "entrypoint"
+  server_id = discord_server.pasokonistan.id
+  category  = discord_category_channel.pasokon.id
+  position  = 0
+}
