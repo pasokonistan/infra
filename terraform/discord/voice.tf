59513fa948cb7235d9a4f3dea880253061a80e9f
sksat <sksat@sksat.net>
2023-03-18 12:54:51 +0900

    discord: add voice channel

--- terraform/discord/voice.tf
+++ terraform/discord/voice.tf
@@ -3,3 +3,10 @@ resource "discord_category_channel" "voice" {
   server_id = discord_server.pasokonistan.id
   position  = 1
 }
+
+resource "discord_voice_channel" "general" {
+  name      = "vc"
+  server_id = discord_server.pasokonistan.id
+  category  = discord_category_channel.voice.id
+  position  = 0
+}
