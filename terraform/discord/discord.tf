0a5c0c6d65573c9454581c5dc51075975baca9e1
sksat <sksat@sksat.net>
2023-03-18 11:41:59 +0900

    add Terraform workspace for Discord

---
+++ terraform/discord/discord.tf
@@ -0,0 +1,17 @@
+terraform {
+  cloud {
+    organization = "pasokonistan"
+
+    workspaces {
+      name = "infra-discord"
+    }
+  }
+
+  required_providers {
+    discord = {
+      source  = "Lucky3028/discord"
+      version = "1.3.0"
+    }
+  }
+}
+
