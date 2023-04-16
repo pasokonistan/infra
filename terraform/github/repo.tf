1a63e55d43c709c1830fbd843d17e36b708333dc
sksat <sksat@sksat.net>
2023-04-17 01:12:23 +0900

    add github_repository.infra current config

--- terraform/github/repo.tf
+++ terraform/github/repo.tf
@@ -1,3 +1,19 @@
 resource "github_repository" "infra" {
-  name = "infra"
+  name        = "infra"
+  description = ""
+  visibility  = "private"
+
+  has_downloads = true
+  has_issues    = true
+  has_projects  = true
+
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+
+  merge_commit_title   = "MERGE_MESSAGE"
+  merge_commit_message = "PR_TITLE"
+
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
 }
