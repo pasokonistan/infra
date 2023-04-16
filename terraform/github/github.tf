6ced1b484464f83f57ccaefe7bf6116cd72d4cdf
sksat <sksat@sksat.net>
2023-04-17 02:39:51 +0900

    terraform import github_organization_settings

--- terraform/github/github.tf
+++ terraform/github/github.tf
@@ -21,3 +21,36 @@ provider "github" {
   # https://registry.terraform.io/providers/integrations/github/latest/docs#github-app-installation
   app_auth {}
 }
+
+resource "github_organization_settings" "pasokonistan" {
+  name        = "pasokonistan"
+  description = "パソコンの国，パソコニスタン"
+
+  billing_email = ""
+  blog          = "https://pasokon.site/"
+
+  default_repository_permission = "read"
+
+  has_organization_projects = true
+  has_repository_projects   = true
+
+  members_can_create_repositories          = true
+  members_can_create_public_repositories   = true
+  members_can_create_private_repositories  = true
+  members_can_create_internal_repositories = false
+  members_can_create_pages                 = true
+  members_can_create_public_pages          = true
+  members_can_create_private_pages         = true
+  members_can_fork_private_repositories    = false
+
+  web_commit_signoff_required = false
+
+  advanced_security_enabled_for_new_repositories = false
+
+  dependabot_alerts_enabled_for_new_repositories           = false
+  dependabot_security_updates_enabled_for_new_repositories = false
+  dependency_graph_enabled_for_new_repositories            = false
+
+  secret_scanning_enabled_for_new_repositories                 = false
+  secret_scanning_push_protection_enabled_for_new_repositories = false
+}
