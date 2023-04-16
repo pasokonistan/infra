d6997b28f3d9ffbbb0a2461be1e3b2357dd59d5d
sksat <sksat@sksat.net>
2023-04-17 01:58:11 +0900

    add pasokonistan team

--- terraform/github/team.tf
+++ terraform/github/team.tf
@@ -1,3 +1,9 @@
+locals {
+  users_paspkonistan = [
+    "sksat",
+  ]
+}
+
 # team
 ## https://docs.github.com/ja/rest/reference/teams#create-a-team--parameters
 ##   privacy=secret: only visible to organization owners and members of this team.
@@ -20,4 +26,13 @@ resource "github_team" "infra_admin" {
   name           = "infra-admin"
   description    = "infra admin"
   privacy        = "closed"
+}
+
+
+resource "github_team_membership" "pasokonistan" {
+  team_id = github_team.pasokonistan.id
+
+  for_each = toset(local.users_paspkonistan)
+  username = each.value
+  role     = "member"
 }
