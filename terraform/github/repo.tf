8ce202edb6da0ca302574fe6f548b9fc12c3d510
sksat <sksat@sksat.net>
2023-04-17 03:48:00 +0900

    terraform import github_repository.pasopedia

--- terraform/github/repo.tf
+++ terraform/github/repo.tf
@@ -52,3 +52,30 @@ resource "github_repository" "pasokon-site" {
 
   vulnerability_alerts = true
 }
+
+resource "github_repository" "pasopedia" {
+  name         = "pasopedia"
+  description  = ""
+  homepage_url = "https://pasopedia.pasokon.site"
+  visibility   = "public"
+
+  has_downloads = true
+  has_issues    = true
+  has_projects  = true
+  has_wiki      = true
+
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+
+  allow_update_branch    = true
+  delete_branch_on_merge = true
+
+  merge_commit_title   = "MERGE_MESSAGE"
+  merge_commit_message = "PR_TITLE"
+
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
+
+  vulnerability_alerts = true
+}
