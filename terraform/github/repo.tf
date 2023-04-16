b1f4a7ee4f73834e05ac437dfa8d25ec9d95b43b
sksat <sksat@sksat.net>
2023-04-17 01:21:05 +0900

    terraform import github_repository.pasokon-site pasokon.site

--- terraform/github/repo.tf
+++ terraform/github/repo.tf
@@ -17,3 +17,26 @@ resource "github_repository" "infra" {
   squash_merge_commit_message = "COMMIT_MESSAGES"
   squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
 }
+
+resource "github_repository" "pasokon-site" {
+  name         = "pasokon.site"
+  description  = "パソコニスタン首都"
+  homepage_url = "https://pasokon.site"
+  visibility   = "public"
+
+  has_downloads = true
+  has_issues    = true
+  has_projects  = true
+
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+
+  merge_commit_title   = "MERGE_MESSAGE"
+  merge_commit_message = "PR_TITLE"
+
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
+
+  vulnerability_alerts = true
+}
